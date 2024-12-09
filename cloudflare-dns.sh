b3c01b7befdd72cf8255046ad16df2711e210fe3
graham <grahamball404@gmail.com>
2024-12-09 18:53:43 +0000

    cloudflare api details ts

--- cloudflare-dns.sh
+++ cloudflare-dns.sh
@@ -14,7 +14,6 @@ check_exit_status() {
     fi
 }
 
-
 # First, try to detect Azure by querying the Azure metadata service
 azure_response=$(curl -s -H "Metadata:true" "http://169.254.169.254/metadata/instance?api-version=2021-02-01")
 
@@ -25,8 +24,8 @@ else
   # If Azure response is not found, check AWS metadata
   aws_response=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
   
-  # Check if AWS response contains "error"
-  if [[ "$aws_response" == *"error"* ]]; then
+  # Check if AWS response is empty or contains "error"
+  if [[ -z "$aws_response" || "$aws_response" == *"error"* ]]; then
     echo "Error: Unable to detect environment (AWS or Azure)."
     exit 1
   else
@@ -34,43 +33,42 @@ else
   fi
 fi
 
+# Ensure required environment variables are set
+if [[ -z "$S_DOMAIN" || -z "$S_PUBLIC_IP" || -z "$CF_API" || -z "$CF_ZONE_ID" ]]; then
+  echo "Error: Required environment variables (S_DOMAIN, S_PUBLIC_IP, CF_API, CF_ZONE_ID) are not set." | tee -a $LOG_FILE
+  exit 1
+fi
+
 # Set the subdomain and IP address for Cloudflare DNS
-domain=S_DOMAIN
-my_domain='${subdomain}.${S_DOMAIN}'
-public_ip=S_PUBLIC_IP
+domain="$S_DOMAIN"
+my_domain="${subdomain}.${S_DOMAIN}"
+public_ip="$S_PUBLIC_IP"
 
 # Cloudflare API and Zone Info
-CF_API=S_CF_API
-CF_ZONE_ID=S_CF_ZONE_ID
-
+CF_API="$CF_API"
+CF_ZONE_ID="$CF_ZONE_ID"
 
 echo "S_DOMAIN: $S_DOMAIN" >> $LOG_FILE
 echo "S_PUBLIC_IP: $S_PUBLIC_IP" >> $LOG_FILE
 echo "CF_API: $CF_API" >> $LOG_FILE
 echo "CF_ZONE_ID: $CF_ZONE_ID" >> $LOG_FILE
 
-
-
 # Create the DNS record in Cloudflare
 sudo echo "Running curl POST request into Cloudflare API..." | tee -a $LOG_FILE
-
-echo "Curl data: {\"content\": \"$public_ip\", \"name\": \"$my_domain\", \"proxied\": true, \"type\": \"A\", \"comment\": \"Automatically adding A record\", \"tags\": [], \"ttl\": 3600}" | tee -a $LOG_FILE
-
 
 curl --request POST \
   --url https://api.cloudflare.com/client/v4/zones/$CF_ZONE_ID/dns_records \
   --header 'Content-Type: application/json' \
   --header "Authorization: Bearer $CF_API" \
-  --data '{
-  "content": "'"$public_ip"'",
-  "name": "'"$my_domain"'",
-  "proxied": true,
-  "type": "A",
-  "comment": "Automatically adding A record",
-  "tags": [],
-  "ttl": 3600
-}'
-
+  --data "{
+  \"content\": \"$public_ip\",
+  \"name\": \"$my_domain\",
+  \"proxied\": true,
+  \"type\": \"A\",
+  \"comment\": \"Automatically adding A record\",
+  \"tags\": [],
+  \"ttl\": 3600
+}"
 
 check_exit_status "Cloudflare DNS API endpoint"
 
