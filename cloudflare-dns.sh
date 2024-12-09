cf9b776929647893bdd3e353b71c429578df5a50
graham <grahamball404@gmail.com>
2024-12-09 19:03:48 +0000

    cf bash update

--- cloudflare-dns.sh
+++ cloudflare-dns.sh
@@ -24,19 +24,13 @@ else
   # If Azure response is not found, check AWS metadata
   aws_response=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
   
-  # Check if AWS response is empty or contains "error"
-  if [[ -z "$aws_response" || "$aws_response" == *"error"* ]]; then
+  # Check if AWS response contains "error"
+  if [[ "$aws_response" == *"error"* ]]; then
     echo "Error: Unable to detect environment (AWS or Azure)."
     exit 1
   else
     subdomain="aws"
   fi
-fi
-
-# Ensure required environment variables are set
-if [[ -z "$S_DOMAIN" || -z "$S_PUBLIC_IP" || -z "$CF_API" || -z "$CF_ZONE_ID" ]]; then
-  echo "Error: Required environment variables (S_DOMAIN, S_PUBLIC_IP, CF_API, CF_ZONE_ID) are not set." | tee -a $LOG_FILE
-  exit 1
 fi
 
 # Set the subdomain and IP address for Cloudflare DNS
