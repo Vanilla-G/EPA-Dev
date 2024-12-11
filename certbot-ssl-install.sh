5e53ab5f1bb923fc5fed919cb52b53f1fad6d6c1
Vanilla-G <126783931+Vanilla-G@users.noreply.github.com>
2024-12-11 14:29:59 +0000

    remove dry run

--- certbot-ssl-install.sh
+++ certbot-ssl-install.sh
@@ -31,8 +31,8 @@ EMAIL=S_EMAIL
 MYDOMAIN="${SUBDOMAIN}.${DOMAIN}"
 
 # Use Certbot to obtain and install the SSL certificate for the specified domain
-#sudo certbot --nginx --non-interactive --agree-tos --email $EMAIL -d $MYDOMAIN -v
-sudo certbot certonly --nginx --agree-tos --dry-run --email $EMAIL -d $DOMAIN -v
+sudo certbot --nginx --non-interactive --agree-tos --email $EMAIL -d $MYDOMAIN -v
+#sudo certbot certonly --nginx --agree-tos --dry-run --email $EMAIL -d $DOMAIN -v
 
 # Nginx unit test that will reload Nginx to apply changes ONLY if the test is successful
 sudo nginx -t && sudo systemctl reload nginx
