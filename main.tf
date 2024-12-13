b55f54b71216cf76292bd22f0253191909d23f95
Vanilla-G <126783931+Vanilla-G@users.noreply.github.com>
2024-12-13 13:22:32 +0000

    aded vpc for dbs and subnet

--- main.tf
+++ main.tf
@@ -11,11 +11,29 @@ provider "aws" {
   region = "eu-west-2"
 }
 
+# Reference the VPC called dbs
+data "aws_vpc" "dbs" {
+  filter {
+    name   = "tag:Name"
+    values = ["dbs"]
+  }
+}
+
+# Reference a subnet within the dbs VPC
+data "aws_subnet_ids" "dbs_subnets" {
+  vpc_id = data.aws_vpc.dbs.id
+}
+
+
 # EC2 instance setup
 resource "aws_instance" "example" {
   ami           = "ami-0e8d228ad90af673b" # Replace with the appropriate AMI ID
   instance_type = "t2.large"
   key_name      = var.key_name
+
+  subnet_id     = data.aws_subnet_ids.dbs_subnets.ids[0]  # Use the first subnet in the VPC
+  security_groups = ["default"] # You can replace with custom security group if necessary
+
 
   tags = {
     Name = "AWS-EPA-instance"
