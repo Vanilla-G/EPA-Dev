79c4d2cd81dff12379a806b0a6440b5def41feed
Vanilla-G <126783931+Vanilla-G@users.noreply.github.com>
2024-12-18 11:31:13 +0000

    Create linpeas-script.sh

---
+++ linpeas-script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+#download linpease from repo 
+sudo wget https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas.sh
+
+# make executable 
+sudo chmod +x linpeas.sh
+
+#sudo bash /home/ubuntu/EPA-Dev-AWS/linpeas.sh | tee linpeas_output.txt
