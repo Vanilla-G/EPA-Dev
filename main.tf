c214e983068050fdb541dddd9896b8485c18f0fd
Vanilla-G <126783931+Vanilla-G@users.noreply.github.com>
2024-12-18 15:58:31 +0000

    Correct Data Source for Subnets

--- main.tf
+++ main.tf
@@ -2,11 +2,12 @@ provider "aws" {
   region = "eu-west-2" # Specify the AWS region
 }
 
+# EC2 instance definition
 resource "aws_instance" "my_ec2_instance" {
   ami           = "ami-05c172c7f0d3aed00"
   instance_type = "t2.micro"
   key_name      = "githubactions"
-  subnet_id     = data.aws_subnet.selected_subnet.id  # Correct reference to data source
+  subnet_id     = data.aws_subnet.selected_subnet.id  # Correct reference to the data source
 
   vpc_security_group_ids = [
     "sg-0e78abc49a200c373"
@@ -19,11 +20,12 @@ resource "aws_instance" "my_ec2_instance" {
 
 # Fetch subnets from the specified VPC
 data "aws_subnets" "available" {
-  vpc_id = "vpc-0ef3faf243858d782"  # Correct data source
+  vpc_id = "vpc-0ef3faf243858d782"  # Correct data source to fetch all subnets
 }
 
+# Selecting a specific subnet from the list of available subnets
 data "aws_subnet" "selected_subnet" {
-  id = data.aws_subnets.available.ids[0]  # Select the first available subnet
+  id = data.aws_subnets.available.ids[0]  # Selecting the first available subnet from the list
 }
 
 # Elastic IP Association (if required)
@@ -37,6 +39,7 @@ data "aws_eip" "existing" {
   id = "eipalloc-0ccc13405e62ed638"  # The Elastic IP you want to use
 }
 
+# Output the instance ID after creation
 output "instance_id" {
   description = "ID of the created EC2 instance"
   value       = aws_instance.my_ec2_instance.id
